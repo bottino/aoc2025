ffce54c2fa440cb7dea0ada22f6924dadcb8e5f4
Baptiste Ottino <baptiste.ottino@gmail.com>
2025-12-02 22:19:30 +0100

    Day 2 - Part 2

--- aoc2025/days/day02.py
+++ aoc2025/days/day02.py
@@ -25,6 +25,33 @@ def part_2(input="example"):
     with open(filepath, "r") as f:
         lines = f.read().splitlines()
 
+    ranges = []
+    for line in lines:
+        ranges.extend([l.split("-") for l in line.split(",") if l != ""])
+
+    invalids = set()
+    for r in ranges:
+        for id in range(int(r[0]), int(r[1]) + 1):
+            str_id = str(id)
+            if _is_invalid_id(str_id):
+                invalids.add(id)
+
+    print(sum(invalids))
+
+
+def _is_invalid_id(id: str) -> bool:
+    for n in range(2, len(id) + 1):
+        if len(id) % n != 0:
+            continue
+
+        m = len(id) // n
+        split_id = [id[i : i + m] for i in range(0, len(id), m)]
+
+        # All elements are the same
+        if len(set(split_id)) == 1:
+            return True
+    return False
+
 
 if __name__ == "__main__":
-    part_1("input")
+    part_2("input")
