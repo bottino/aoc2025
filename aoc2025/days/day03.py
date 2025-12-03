dc03cb7124f0f3efb7a857c49567ad2c7322cc0d
Baptiste Ottino <baptiste.ottino@gmail.com>
2025-12-03 21:42:31 +0100

    Day 3 - Part 2 - Stack method

--- aoc2025/days/day03.py
+++ aoc2025/days/day03.py
@@ -30,43 +30,23 @@ def part_2(input="example"):
     total_joltage = 0
     num_batteries = 12
     for line in lines:
-        digits = [0] * num_batteries
+        stack = []
         for i, c in enumerate(line):
             num = int(c)
-            for j, n in enumerate(digits):
-                if num > n and i < len(line) - (num_batteries - j - 1):
-                    digits[j] = num
-                    digits[j + 1 :] = [0] * len(digits[j + 1 :])
-                    break
+            while (
+                stack
+                and stack[-1] < num
+                and len(stack) + (len(line) - i) > num_batteries
+            ):
+                stack.pop()
+            if len(stack) < num_batteries:
+                stack.append(num)
 
-        joltage = sum([10**i * n for i, n in enumerate(digits[::-1])])
-        total_joltage += joltage
-
-    print(total_joltage)
-
-
-def part_2_alternate(input="example"):
-    filepath = f"./inputs/day03/{input}.txt"
-    with open(filepath, "r") as f:
-        lines = f.read().splitlines()
-
-    total_joltage = 0
-    num_batteries = 12
-    for line in lines:
-        slice = [int(c) for c in line]
-        digits = [0] * num_batteries
-        for i in range(num_batteries):
-            tail_len = num_batteries - i - 1
-            s = slice[:-tail_len] if tail_len != 0 else slice[:]
-            idx = s.index(max(s))
-            digits[i] = s[idx]
-            slice = slice[idx + 1 :]
-
-        joltage = sum([10**i * n for i, n in enumerate(digits[::-1])])
+        joltage = sum([10**i * n for i, n in enumerate(stack[::-1])])
         total_joltage += joltage
 
     print(total_joltage)
 
 
 if __name__ == "__main__":
-    part_2_alternate("input")
+    part_2("input")
