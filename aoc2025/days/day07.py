834b46a338ab9e0fb919ec3720a143f1be26211d
Baptiste Ottino <baptiste.ottino@gmail.com>
2025-12-10 00:43:33 +0100

    Day 7 - Compact

--- aoc2025/days/day07.py
+++ aoc2025/days/day07.py
@@ -4,7 +4,13 @@ def part_1(input: str) -> int:
     beams = {start_column}
     num_splits = 0
     for row in manifold[1:]:
-        beams, num_splits = _process_row(beams, row, num_splits)
+        for i, char in enumerate(row):
+            if char == "^" and i in beams:
+                num_splits += 1
+                beams.remove(i)
+                beams.add(i - 1)
+                beams.add(i + 1)
+
     return num_splits
 
 
@@ -14,27 +20,11 @@ def part_2(input: str) -> int:
     timelines = [0] * len(manifold[0])
     timelines[start_column] = 1
     for row in manifold[1:]:
-        timelines = _propagate_timelines(timelines, row)
+        for i, char in enumerate(row):
+            if char == "^":
+                num_timelines = timelines[i]
+                timelines[i] = 0
+                timelines[i - 1] += num_timelines
+                timelines[i + 1] += num_timelines
 
     return sum(timelines)
-
-
-def _process_row(beams: set[int], row: list[str], num_splits: int) -> [set[int], int]:
-    for i, char in enumerate(row):
-        if char == "^":
-            if i in beams:
-                num_splits += 1
-                beams.remove(i)
-                beams.add(i - 1)
-                beams.add(i + 1)
-    return beams, num_splits
-
-
-def _propagate_timelines(beams: list[int], row: list[str]) -> dict[int, int]:
-    for i, char in enumerate(row):
-        if char == "^":
-            num_timelines = beams[i]
-            beams[i] = 0
-            beams[i + 1] += num_timelines
-            beams[i - 1] += num_timelines
-    return beams
