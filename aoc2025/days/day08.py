9dd37e9f8270f1c642ad2f2bbc2ad4ab2382ae89
Baptiste Ottino <baptiste.ottino@gmail.com>
2025-12-09 23:53:54 +0100

    Day 8 - Part 2

--- aoc2025/days/day08.py
+++ aoc2025/days/day08.py
@@ -6,15 +6,7 @@ Connection = tuple[int, tuple[Box, Box]]
 
 
 def part_1(input: str) -> int:
-    boxes = _read_boxes(input)
-    connections: set[Connection] = set()
-    for a in boxes:
-        for b in boxes:
-            if a != b:
-                pair = tuple(sorted((a, b)))
-                connections.add((_dist(a, b), pair))
-    connections = sorted(connections)
-
+    boxes, connections = _prepare_connections(input)
     MAX_CONNECTIONS = 10 if len(boxes) == 20 else 1000  # example vs input
     pairs = [c[1] for c in connections[:MAX_CONNECTIONS]]
 
@@ -31,6 +23,32 @@ def part_1(input: str) -> int:
     for cc in largest_cc[:NUM_LARGEST_CIRCUITS]:
         solution *= cc
     return solution
+
+
+def part_2(input: str) -> int:
+    boxes, connections = _prepare_connections(input)
+    G = nx.Graph()
+    G.add_nodes_from(boxes)
+
+    i = -1
+    while not nx.is_connected(G):
+        i += 1
+        G.add_edge(*connections[i][1])
+
+    last_pair = connections[i][1]
+    return last_pair[0][0] * last_pair[1][0]
+
+
+def _prepare_connections(input: str) -> [list[Box], list[Connection]]:
+    boxes = _read_boxes(input)
+    connections: set[Connection] = set()
+    for a in boxes:
+        for b in boxes:
+            if a != b:
+                pair = tuple(sorted((a, b)))
+                connections.add((_dist(a, b), pair))
+    connections = sorted(connections)
+    return boxes, connections
 
 
 def _read_boxes(input: str) -> list[Box]:
